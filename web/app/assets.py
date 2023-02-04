8f057b3292e3c3be4cf60f1436bf8f9d11d1ee1e
Patrick van der Leer <pat.vdleer@gmail.com>
2023-02-04 22:56:37 +0100

    Initial attempt at preset modal

--- web/app/assets.py
+++ web/app/assets.py
@@ -14,7 +14,9 @@ class ConcatFilter(Filter):
 
 
 js = Bundle(
+    'https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js',
+    'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js',
     'js/application.js',
     filters=(ConcatFilter, 'jsmin'),
     output='gen/packed.js'
@@ -22,6 +24,8 @@ js = Bundle(
 
 css = Bundle(
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
+    "https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css",
+    "https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css",
     'css/fonts.css',
     'css/style.css',
     filters=('cssmin', 'cssrewrite'),
