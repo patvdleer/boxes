0c54c8d08e15cf2f18e469b77e359b6adca829d1
Patrick van der Leer <pat.vdleer@gmail.com>
2023-02-05 15:05:28 +0100

    Fix category bug in presets

--- tests/test_presets.py
+++ tests/test_presets.py
@@ -1,4 +1,5 @@
 import unittest
+from dataclasses import dataclass
 
 
 class TestRegistry(unittest.TestCase):
@@ -18,3 +19,25 @@ class TestPresetConfig(unittest.TestCase):
         can_storage = CanStorage()
         can_storage.presets.categorised()
 
+
+class TestProduct(unittest.TestCase):
+    def test_product_categories(self):
+        from boxes.presets.shapes import Cylinder
+        from boxes.presets import Product
+
+        @dataclass
+        class TestProductWOCat(Product, Cylinder):
+            pass
+
+        @dataclass
+        class TestProductWCat(Product, Cylinder):
+            categories = [
+                "Battery"
+            ]
+
+        prod_wo_cat = TestProductWOCat(name="07540", diameter=7.5, height=40)
+        assert isinstance(prod_wo_cat.categories, list)
+
+        prod_w_cat = TestProductWCat(name="07540", diameter=7.5, height=40)
+        assert isinstance(prod_w_cat.categories, list)
+        assert "Battery" in prod_w_cat.categories
